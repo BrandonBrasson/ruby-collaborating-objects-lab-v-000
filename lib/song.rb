112793398cafb81c038562919c6d5c2576865e9f
Brandon Brasson <rocksong622@gmail.com>
2019-03-20 15:49:52 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+attr_accessor :name,:artist,:song
+@@all=[]
+
+def initialize(name)
+  @name = name
+  @song = []
+end
+
+def songs
+  @artist=[]
+end
+
+
+  def self.new_by_filename(filename)
+  @@all=[]
+  end
+
+
+
+
+end
+#def self.new_by_filename(file_name)
+  #@songs.find do |song|
+  #puts song.name
+  #  end
+  #end
+
+    #    Song.new_by_filename(filename)
+    #  end
+#end
