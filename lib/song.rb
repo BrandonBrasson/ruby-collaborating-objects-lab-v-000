dae908efacb9ddaea8e0b112d1e7540429bd5d2a
Brandon Brasson <rocksong622@gmail.com>
2019-03-20 16:35:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ def self.new_by_filename(file_name)
    song = Song.new(name)
    song.artist =  Artist.new(artist)
    song.artist.songs << []
-   artist = Artist.all.size 
+   artist = Artist.all.size.to eq (3)
    if artist
      puts song.artist.save
    end
