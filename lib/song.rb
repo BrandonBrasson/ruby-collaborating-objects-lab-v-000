da2c0389f31ddce69ef9138d4075bdc221fed739
Brandon Brasson <rocksong622@gmail.com>
2019-03-20 16:13:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,15 +12,18 @@ def songs
 end
 
 def self.new_by_filename(new_file)
-
    name = new_file.split(" - ")[1]
    artist = new_file.split(" - ")[0]
    song = Song.new(name)
    song.artist =  Artist.new(artist)
    song.artist.songs << song
+   duplicate = Artist.all.detect {|i| i.name == artist }
+   if !duplicate
      song.artist.save
    end
-
+   song
+   # song.artist = artist
+ end
 #  def self.new_by_filename(new_file)
   #  @@all.each do |song|
   #  if song.name == @song
